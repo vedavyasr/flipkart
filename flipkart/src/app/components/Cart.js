d712f28522123a6063bf1bd997062eb91bfe11f3
Vinothini Rajendran <M1037952@mindtree.com>
2019-02-06 12:21:44 +0530

    with cart

--- flipkart/src/app/components/Cart.js
+++ flipkart/src/app/components/Cart.js
@@ -1,5 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { deleteProduct } from '../state/actions'
+import { connect } from 'react-redux'
 
-export default ()=>(<div>
-    Cart
-</div>)
+class Cart extends Component {
+    render() {
+        console.log(this.props)
+        return (
+            <div className="container">
+                <div>
+                    <h1>Cart</h1>
+                    <table>
+                        <tbody>
+                            {
+                                this.props.cart.map((product, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td >{product.name}</td>
+                                            <td >{product.id}</td>
+                                            <td>{product.price}</td>
+                                            <td><button onClick={() => this.props.deleteProduct(product.id)}>Remove</button></td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
+
+let mapStateToProps = (state) => {
+    return {
+        cart: state.products.cart
+    };
+}
+
+export default connect(mapStateToProps, { deleteProduct })(Cart);
