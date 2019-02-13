827bf008545ec03cc08beeea32de746477d2b7c7
Vedavyas Reddi <M1043143@mindtree.com>
2019-02-13 20:00:40 +0530

    changes in test

--- flipkart/src/__tests/Reducer.test.js
+++ flipkart/src/__tests/Reducer.test.js
@@ -1,29 +1,43 @@
-import React from "react";
-import CartReducer from "../app/state/reducers/cartReducer";
-import thunk from "redux-thunk";
+import ProductReducer from "../app/state/reducers/productReducer";
 import * as actions from "../app/state/action-types";
-import Cart from "../app/components/Cart";
-import Adapter from "enzyme-adapter-react-16";
-import Enzyme, { mount, shallow } from "enzyme";
-import { Provider } from "redux";
-import configureStore from "../app/configureStore";
-import configuremockstore from "redux-mock-store";
 
-const middlewares = [thunk];
-
-const mockStore = configuremockstore(middlewares);
-// let store = configureStore();
-const store = mockStore({ cart: [], cartItems: [], total: 0 });
-Enzyme.configure({ adapter: new Adapter() });
-function setup() {
-  const enzymeWrapper = shallow(<Cart />);
-  return {
-    enzymeWrapper
-  };
-}
-describe("CartReducer ", () => {
-  it("test cart empty list", () => {
-    const { enzymeWrapper } = setup();
-    expect(enzymeWrapper.find("tr").length).toBe(0);
+describe("Product reducer", () => {
+  it("should return the initial state", () => {
+    expect(ProductReducer(undefined, {})).toEqual({
+      isFetching: false,
+      products: [],
+      error: {},
+      searchValue: "",
+      results: [],
+      productDetail: [],
+      productsToDisplay: []
+    });
+  });
+  it("should add something to store ", () => {
+    expect(
+      ProductReducer(
+        {
+          isFetching: false,
+          products: [],
+          error: {},
+          searchValue: "",
+          results: [],
+          productDetail: [],
+          productsToDisplay: []
+        },
+        {
+          type: actions.FETCHING_PRODUCTS_SUCCESS,
+          payload: [{ id: 1, name: "name" }]
+        }
+      )
+    ).toEqual({
+      isFetching: false,
+      products: [{ id: 1, name: "name" }],
+      error: {},
+      searchValue: "",
+      results: [],
+      productDetail: [],
+      productsToDisplay: [{ id: 1, name: "name" }]
+    });
   });
 });
